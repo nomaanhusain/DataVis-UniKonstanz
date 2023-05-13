1f1c96a93278babeff32111a12a686a789b9ab2c
nomaanhusain <nomaanhusain@gmail.com>
2023-05-13 18:44:16 +0200

    Part 1 complete

--- Assingment03/histogram/index.js
+++ Assingment03/histogram/index.js
@@ -17,17 +17,21 @@ const margin = {
   right: 20,
   bottom: 50,
 };
-
+let rects = null
+let axisX = null
+let axisY = null
 // We select the slider #bins-slider and add a input listener that adjusts
 // the value of the <span> #bins-counter to the currently selected value and calls the method update()
 // For more details, see https://github.com/d3/d3-selection#selection_on
 d3.select("#bins-slider").on("input", (e) => {
   const val = e.srcElement.valueAsNumber;
   d3.select("#bins-counter").text(e.srcElement.valueAsNumber);
+  rects.transition().duration(600).style("opacity", 0).remove()
+  axisX.transition().duration(200).style("opacity", 0).remove()
+  axisY.transition().duration(200).style("opacity", 0).remove()
   update(val);
 });
 
-let allData = data
 // TASK 2: Write a function 'update', that realizes the general update pattern of d3 to create a histogram of the data using the currently selected number of bins.
 // The update function has one parameter 'numbins' that describes how many bins are currently selected in the #bins-slider <input> element.
 // The update function should create a bar chart in the <svg> element with the id #chart
@@ -39,7 +43,7 @@ function update(numbins) {
   // Make sure to specify the following attributes: domain and threshold, threshold is the approximate number of bins.
   // Afterwards bin the input data
   console.log(d3.max(data, function(d) { return d.rate; }))
-  const bin_function = d3.bin().domain([0,d3.max(data, function(d) { return d.rate; })]).thresholds(20)
+  const bin_function = d3.bin().domain([0,d3.max(data, function(d) { return d.rate; })]).thresholds(numbins)
   var ratesValue = data.map(function(d) {
     return d.rate
   });
@@ -47,41 +51,54 @@ function update(numbins) {
   const bins = bin_function(ratesValue);
   console.log("Bins = ",bins)
   // Task 2.3 Create a linear y scale to given the number of elements in the bins [0, max(elements in bin)]
-   let yScale = d3.scaleLinear().domain([0,d3.max(bins, function(d) { return d.length; })]).range([height,0]).nice()
-   let xScale = d3.scaleLinear().domain([1,d3.max(bins, (d) => d.x0)]).range([margin.left,width-margin.right])
+   let yScale = d3.scaleLinear().domain([0,d3.max(bins, function(d) { return d.length; })]).range([height,margin.bottom])
+   let xScale = d3.scaleLinear().domain([0,bins.length]).range([margin.left,width-margin.right-margin.left])
   // TASK 2.4. Select the #chart <svg> element.
   const svg = d3.select("#chart")
   .append("svg")
-    .attr("width", width+margin.left+margin.right)
-    .attr("height", height+margin.bottom)
+    .attr("width", (width+margin.left+margin.right))
+    .attr("height", (height+margin.bottom))
   .append("g")
     .attr("transform", "translate(0,0)");
 
   // TASK 2.5 Remove old rectangles that are no longer needed using either selection.remove() or exit()
-  //...
+  //
 
   // TASK 2.6. Connect all <rect> children (using data and enter) of the svg with the bins (be careful to use the length of the bins, not the bin elements)
   let idx=0
-  var toType = function(obj) {
+  //test function, ignore
+  var findType = function(obj) {
     return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
   }
-  let eachBinWidth = (width - margin.left - margin.right)/numbins
+
   bins.forEach(element => {
-    console.log("yee "+ idx +" "+ yScale(element.length))
+    // console.log("yee "+ idx +" "+ yScale(element.length))
     idx=idx+1;
   });
   
 
-  const rects = svg.selectAll("rect")
+  rects = svg.selectAll("rect")
   .data(bins)
   .enter()
   .append("rect")
     .attr("x", 0)
     .attr("y",0)
-    .attr("transform", function(d) { return "translate(" + xScale(d.x0)  + "," + yScale(d.length) + ")"; }) //xScale(d.x0) 
-    .attr("width", function(d){return xScale(d.x1)-xScale(d.x0) - 1})
+    .attr("transform", function(d) { return "translate(" + (xScale(d.x0))  + "," + (yScale(d.length)-margin.bottom) + ")"; }) //xScale(d.x0) 
+    .attr("width", function(d){return xScale(d.x1)-xScale(d.x0)})
     .attr("height", function(d) { return height - yScale(d.length); })
     .style("fill", "#69b3a2")
+
+// rects.append('g').attr("transform", "translate(0,10)");
+  
+axisX=svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate("+(0)+","+(height-margin.bottom)+")")
+    .call(d3.axisBottom(xScale));
+
+axisY=svg.append("g")
+  .attr("class", "y axis")
+  .attr("transform", "translate("+margin.left+","+(-margin.bottom)+")")
+  .call(d3.axisLeft(yScale));
 
   // Task 3 Add a tooltip to the visualization
   // Hint:
